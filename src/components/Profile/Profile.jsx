2f1ee9cde5872e28c73894a5c0f2677a01e86076
rishabh0111 <rishabhsharma8912@gmail.com>
2023-10-08 22:58:31 +0530

    implemented authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { userSelector } from '../../features/auth';
+import { Box, Button, Typography } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 
 const Profile = () => {
-    console.log("Profile");
+    const {user} = useSelector(userSelector);
+    const favoriteMovies = [];
+
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = '/';
+    };
+
     return (
-        <div>Profile</div>
+        <Box>
+            <Box display='flex' justifyContent="space-between">
+                <Typography variant='h4' gutterBottom>My Profile</Typography>
+                <Button color="inherit" onClick={logout}>
+                    Logout &nbsp; <ExitToApp />
+                </Button>
+            </Box>
+            {!favoriteMovies.length
+            ? <Typography variant="h5">Add favorites or watchlist some movies to see them here!</Typography>
+            : (<Box>
+                FAVORITE Movies
+            </Box>
+            )}
+        </Box>
     )
 }
 
